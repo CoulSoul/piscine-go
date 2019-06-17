4c0d02d40477983309aa18c940e09b86def1d3b8
CoulSoul <49014506+CoulSoul@users.noreply.github.com>
2019-06-17 14:38:10 +0000

    Update printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -3,10 +3,10 @@ package piscine
 import "fmt"
 
 func PrintComb()  {
-	for i:= {
-		for j := 1; j:= 9; j++{
+	for i:= 0 {
+		for j := 1; j<= 9; j++ {
 			fmt.Printf(j)
-			for k := 3; k :=9; k++{ 
+			for k := 3; k <=9; k++ {  
 				fmt.Printf(k)
 			}
 		}
