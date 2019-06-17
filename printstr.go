c1105472761d8daab8bc74810545c1325adb4ee9
Alloco225 <39595564+Alloco225@users.noreply.github.com>
2019-06-17 13:58:26 +0000

    My commit

--- printstr.go
+++ printstr.go
@@ -1,11 +1,12 @@
 package piscine
 
-import "fmt"
 
 func PrintStr(str string)  {
 	
-	for i := 0; i < len(str); i++{
-		fmt.Println(string(str[i]))
+	for _, v := range str{
+		print(string(v))
 	}
 
 }
+
+
